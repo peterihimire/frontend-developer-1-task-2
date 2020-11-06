1ebd1f2b2498670b3ac603191dad224d9c71cfb9
Peter Ihimire <peterihimire@gmail.com>
2020-11-06 18:03:25 +0100

    Trying to setup React Hook useState and useEffect for this project

--- src/App.js
+++ src/App.js
@@ -1,114 +1,166 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Banner from "./components/Banner";
 // import Footer from "./components/Footer";
-import BookList from "./components/BookList";
+// import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import MobileNav from "./components/MobileNav";
-import Form from "./components/Form";
+// import Form from "./components/Form";
 
-class App extends Component {
-  state = {
-    books: [],
-    sortedBooks: [],
-    loading: true,
-    bookOne: "",
+const App = (props) => {
+  // const [booksState, setBooksState] = useState({
+  //   books: [],
+  // });
+  // const [bookOne, setBookOneState] = useState({
+  //   bookOne: "",
+  // });
+
+  // For Menu Toggle
+  const [menuState, setMenuState] = useState({
     isOpen: false,
-  };
+  });
 
-  // menu handler
-  openHandler = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
+  const openHandler = () => {
+    setMenuState({
+      isOpen: !menuState.isOpen,
     });
   };
-  closeHandler = () => {
-    this.setState({
+  const closeHandler = () => {
+    setMenuState({
       isOpen: false,
     });
   };
-
-  // Form submit Handler
-  handleSubmit = (event) => {
-    event.preventDefault();
-    // console.log(bookOne);
-
-    fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=isbn:${this.state.bookOne}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-
-        let bookList = data.items.map((item) => {
-          let id = item.id;
-          let authors = item.volumeInfo.authors;
-          let rating = item.volumeInfo.averageRating;
-          let categories = item.volumeInfo.categories;
-          let description = item.volumeInfo.description;
-          let language = item.volumeInfo.language;
-          let pageCount = item.volumeInfo.pageCount;
-          let publishedDate = item.volumeInfo.publishedDate;
-          let publisher = item.volumeInfo.publisher;
-          let title = item.volumeInfo.title;
-
-          let book = {
-            authors,
-            id,
-            rating,
-            categories,
-            description,
-            language,
-            pageCount,
-            publishedDate,
-            publisher,
-            title,
-          };
-          console.log(book);
-          return book;
-        });
-        // console.log(bookList)
-        return bookList;
-      })
-
-      .then((booksData) => {
-        console.log(booksData);
-        this.setState(() => {
-          return {
-            books: booksData,
-          };
-        });
-      })
-      .catch((err) => console.log(err));
-  };
-
-  // Form input Handler
-  handleChange = (event) => {
-    event.preventDefault();
-    const bookg = event.target.value;
-
-    this.setState({ bookOne: bookg });
-    console.log(bookg);
-  };
-
-  render() {
-    return (
+  useEffect(() => {});
+  return (
+    <div>
       <>
-        <Navbar openMenu={this.openHandler} />
+        <Navbar openMenu={openHandler} />
         <MobileNav
-          closeMenu={this.closeHandler}
-          openState={this.state.isOpen}
+          closeMenu={closeHandler}
+          openState={menuState.isOpen}
         />
         <Banner />
-        <Form forInput={this.handleChange} forForm={this.handleSubmit} />
-        <BookList bookInfo={this.state.books} />
+        {/* <Form forInput={this.handleChange} forForm={this.handleSubmit} />
+        <BookList bookInfo={this.state.books} /> */}
         {/* <Footer /> */}
       </>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
+
+// import React, { Component } from "react";
+// import "./App.css";
+// import Banner from "./components/Banner";
+// // import Footer from "./components/Footer";
+// import BookList from "./components/BookList";
+// import Navbar from "./components/Navbar";
+// import MobileNav from "./components/MobileNav";
+// import Form from "./components/Form";
+
+// class App extends Component {
+//   state = {
+//     books: [],
+//     sortedBooks: [],
+//     loading: true,
+//     bookOne: "",
+//     isOpen: false,
+//   };
+
+//   // menu handler
+//   openHandler = () => {
+//     this.setState({
+//       isOpen: !this.state.isOpen,
+//     });
+//   };
+//   closeHandler = () => {
+//     this.setState({
+//       isOpen: false,
+//     });
+//   };
+
+//   // Form submit Handler
+//   handleSubmit = (event) => {
+//     event.preventDefault();
+//     // console.log(bookOne);
+
+//     fetch(
+//       `https://www.googleapis.com/books/v1/volumes?q=isbn:${this.state.bookOne}`
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data);
+
+//         let bookList = data.items.map((item) => {
+//           let id = item.id;
+//           let authors = item.volumeInfo.authors;
+//           let rating = item.volumeInfo.averageRating;
+//           let categories = item.volumeInfo.categories;
+//           let description = item.volumeInfo.description;
+//           let language = item.volumeInfo.language;
+//           let pageCount = item.volumeInfo.pageCount;
+//           let publishedDate = item.volumeInfo.publishedDate;
+//           let publisher = item.volumeInfo.publisher;
+//           let title = item.volumeInfo.title;
+
+//           let book = {
+//             authors,
+//             id,
+//             rating,
+//             categories,
+//             description,
+//             language,
+//             pageCount,
+//             publishedDate,
+//             publisher,
+//             title,
+//           };
+//           console.log(book);
+//           return book;
+//         });
+//         // console.log(bookList)
+//         return bookList;
+//       })
+
+//       .then((booksData) => {
+//         console.log(booksData);
+//         this.setState(() => {
+//           return {
+//             books: booksData,
+//           };
+//         });
+//       })
+//       .catch((err) => console.log(err));
+//   };
+
+//   // Form input Handler
+//   handleChange = (event) => {
+//     event.preventDefault();
+//     const bookg = event.target.value;
+
+//     this.setState({ bookOne: bookg });
+//     console.log(bookg);
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <Navbar openMenu={this.openHandler} />
+//         <MobileNav
+//           closeMenu={this.closeHandler}
+//           openState={this.state.isOpen}
+//         />
+//         <Banner />
+//         <Form forInput={this.handleChange} forForm={this.handleSubmit} />
+//         <BookList bookInfo={this.state.books} />
+//         {/* <Footer /> */}
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
 
 // const getBook = () => {
 //   fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}')
