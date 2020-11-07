c9a002e74c6d31cc82281f091f4896962acb1807
Peter Ihimire <peterihimire@gmail.com>
2020-11-07 13:59:03 +0100

    Refactored the code to use React-Hooks for functional component instead of the normal life-cycle-hooks we use in class based component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Banner from "./components/Banner";
 // import Footer from "./components/Footer";
-// import BookList from "./components/BookList";
+import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import MobileNav from "./components/MobileNav";
-// import Form from "./components/Form";
+import Form from "./components/Form";
 
 const App = (props) => {
-  // const [booksState, setBooksState] = useState({
-  //   books: [],
-  // });
-  // const [bookOne, setBookOneState] = useState({
-  //   bookOne: "",
-  // });
+  const [booksState, setBooksState] = useState({
+    books: [],
+  });
+  const [bookOne, setBookOneState] = useState({
+    bookOne: "",
+  });
 
   // For Menu Toggle
   const [menuState, setMenuState] = useState({
@@ -30,18 +30,77 @@ const App = (props) => {
       isOpen: false,
     });
   };
-  useEffect(() => {});
+
+  // For Form and Search
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=isbn:${bookOne.bookOne}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        let bookList = data.items.map((item) => {
+          let id = item.id;
+          let authors = item.volumeInfo.authors;
+          let rating = item.volumeInfo.averageRating;
+          let categories = item.volumeInfo.categories;
+          let description = item.volumeInfo.description;
+          let language = item.volumeInfo.language;
+          let pageCount = item.volumeInfo.pageCount;
+          let publishedDate = item.volumeInfo.publishedDate;
+          let publisher = item.volumeInfo.publisher;
+          let title = item.volumeInfo.title;
+
+          let book = {
+            authors,
+            id,
+            rating,
+            categories,
+            description,
+            language,
+            pageCount,
+            publishedDate,
+            publisher,
+            title,
+          };
+          console.log(book);
+          return book;
+        });
+        // console.log(bookList)
+        return bookList;
+      })
+
+      .then((booksData) => {
+        console.log(booksData);
+        setBooksState(() => {
+          return {
+            books: booksData,
+          };
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const changeHandler = (e) => {
+    e.preventDefault();
+
+    const bookg = e.target.value;
+
+    setBookOneState({ bookOne: bookg });
+    console.log(bookg);
+  };
+
   return (
     <div>
       <>
         <Navbar openMenu={openHandler} />
-        <MobileNav
-          closeMenu={closeHandler}
-          openState={menuState.isOpen}
-        />
+        <MobileNav closeMenu={closeHandler} openState={menuState.isOpen} />
         <Banner />
-        {/* <Form forInput={this.handleChange} forForm={this.handleSubmit} />
-        <BookList bookInfo={this.state.books} /> */}
+        <Form forInput={changeHandler} forForm={submitHandler} />
+        <BookList bookInfo={booksState.books} />
         {/* <Footer /> */}
       </>
     </div>
